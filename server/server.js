ec5ed6baafe7f567a274d74474b288266233cd88
koushikkumarkadari <139983437+koushikkumarkadari@users.noreply.github.com>
2025-04-25 10:19:14 +0530

    Enable CORS preflight requests for all routes

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(cors({
   credentials: true,
 }));
 app.use(express.json());
-app.options('*', cors());
+
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
