20302c504ab9dbd26d49a976045ac139a415d382
koushikkumarkadari <139983437+koushikkumarkadari@users.noreply.github.com>
2025-04-25 10:14:45 +0530

    Refactor CORS middleware configuration for improved security and clarity

--- server/server.js
+++ server/server.js
@@ -7,9 +7,13 @@ const jwt = require('jsonwebtoken');
 
 dotenv.config();
 const app = express();
-app.use(cors());
-app.use(express.json());
 
+app.use(cors({
+  origin: 'https://dental-checkup-app-frontend.onrender.com',
+  credentials: true,
+}));
+app.use(express.json());
+app.options('*', cors());
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
